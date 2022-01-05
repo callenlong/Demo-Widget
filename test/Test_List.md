645f4a81b969b1e6e774c142a4628d8f7b553161
callenlong <callenlong@gmail.com>
2022-01-04 21:31:23 -0500

    moved LED unit test code to test_led_driver.c/.h

---
+++ test/Test_List.md
@@ -0,0 +1,9 @@
+## Test List for Demonstration Widget
+
+The following is the list of unit tests for each of the peripherals on the microcontroller and external device drivers.
+
+#LED DRIVER
+- Initialize LEDs
+- De-Initialize LEDs 
+- A single LED can be turned on/off
+- Check each LED state
--- test/test_main.c
+++ test/test_main.c
@@ -2,31 +2,11 @@
 #include <unity.h>
 
 void setUp(void) {
-    LED_GPIO_CLK_ENABLE();
-    GPIO_InitTypeDef GPIO_InitStruct;
-    GPIO_InitStruct.Pin = LED_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-    HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+    LED_Init();
 }
 
 void tearDown(void) {
-    HAL_GPIO_DeInit(LED_GPIO_PORT, LED_PIN);
-}
-
-void test_led_builtin_pin_number(void) {
-    TEST_ASSERT_EQUAL(GPIO_PIN_5, LED_PIN);
-}
-
-void test_led_state_high(void) {
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED_PIN, GPIO_PIN_SET);
-    TEST_ASSERT_EQUAL(GPIO_PIN_SET, HAL_GPIO_ReadPin(LED_GPIO_PORT, LED_PIN));
-}
-
-void test_led_state_low(void) {
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED_PIN, GPIO_PIN_RESET);
-    TEST_ASSERT_EQUAL(GPIO_PIN_RESET, HAL_GPIO_ReadPin(LED_GPIO_PORT, LED_PIN));
+    LED_DeInit();
 }
 
 int main() {
